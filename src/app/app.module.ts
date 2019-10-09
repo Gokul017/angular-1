f4b7b69e4444c35b19c76c5c1d5f9d423a56f576
Gokul017 <choudharygk108@gmail.com>
2019-10-09 17:53:00 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { CalcComponent } from './calc/calc.component';
+
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, CalcComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:   'g-calc',
+  templateUrl:'../calc/calc.component.html',
+  styleUrls:['../calc/calc.component.css'] 
+})
+
+export class CalcComponent{
+  calcName = 'CalCalc';
+}
